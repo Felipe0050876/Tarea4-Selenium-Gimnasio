6d5617743c25e7826115b8f57e96b12a45a7edcd
felipe <felipediaz1330@gmail.com>
2026-04-02 20:09:28 -0400

    Agregar archivos de proyecto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroMiembros.Models;
+
+namespace RegistroMiembros.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Miembro> Miembros { get; set; }
+    }
+}
---
+++ Models/Miembro.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroMiembros.Models
+{
+    public class Miembro
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        public string Apellido { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Telefono { get; set; }
+    }
+}
